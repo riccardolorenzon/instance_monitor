6bbf9b0d83576f1f71ce96bdcd73d30479ec90b1
riccardo lorenzon <riccardo.lorenzon@gmail.com>
2015-03-10 22:39:31 +0100

    added vent, unit tests

--- instance_monitor.py
+++ instance_monitor.py
@@ -1,6 +1,10 @@
 # todo unittests
 
 #todo use namedvalue instead
+
+from collections import namedtuple
+import os
+
 class instance_detail():
     def __init__(self, type_name):
         self.type_name = type_name
@@ -9,66 +13,74 @@ class instance_detail():
         self.min_free_slots_number_per_host = -1
         self.hosts_with_min_free_slots_number = 0
 
-def make_stats(file_input_path, file_output_path):
+def process_record(number_occupied_slots, slot_number, instance_type_count_dict ):
+    if number_occupied_slots == 0:
+        instance_type_count_dict.empty_hosts += 1
+    if number_occupied_slots == slot_number:
+        instance_type_count_dict.full_hosts += 1
+    instance_type_count_dict.occupied_slots = number_occupied_slots
+    if instance_type_count_dict.min_free_slots_number_per_host == -1 or \
+        instance_type_count_dict.min_free_slots_number_per_host < slot_number - number_occupied_slots:
+        instance_type_count_dict.min_free_slots_number_per_host, instance_type_count_dict.hosts_with_min_free_slots_number = slot_number - number_occupied_slots, 1
+    elif instance_type_count_dict.min_free_slots_number_per_host == slot_number - number_occupied_slots:
+        instance_type_count_dict.hosts_with_min_free_slots_number += 1
+
+def analyze_records(fr, write_func):
     instance_count_dict = {}
+
     instance_count_dict['M1'] = instance_detail('M1')
     instance_count_dict['M2'] = instance_detail('M2')
     instance_count_dict['M3'] = instance_detail('M3')
-    print 'working..'
-    #todo check if file_input and file_output are correct
-    with open(file_input_path, 'r') as fr:
-        for host_line in fr:
-            #TODO use list comphrension
-            
-            # get an associative array(dict) for the current fleet record
-            host_array = host_line.strip().split(',')
-            # check array length
-            if len(host_array) < 4 and len(host_array) > 0:
-                raise ValueError("too few values for host ID: %s " % host_array[0])
-            elif len(host_array) < 0:
-                raise ValueError("one record corrupted, check FleetState.txt")
-            instance_type = host_array[1]
-            slot_number = int(host_array[2])
-            try:
-                #TODO use function host_array[3:].count('0')
-                
-                number_occupied_slots = sum(map(int, host_array[3:]))
-            except ValueError:
-                raise ValueError("check values for host ID: %s " % host_array[0])
-                
-            # todo move out this logic
-            if number_occupied_slots == 0:
-                instance_count_dict[instance_type].empty_hosts += 1
-            if number_occupied_slots == slot_number:
-                instance_count_dict[instance_type].full_hosts += 1
-            instance_count_dict[instance_type].occupied_slots = number_occupied_slots
-            if instance_count_dict[instance_type].min_free_slots_number_per_host == -1 or \
-                instance_count_dict[instance_type].min_free_slots_number_per_host < slot_number - number_occupied_slots:
-                instance_count_dict[instance_type].min_free_slots_number_per_host = slot_number - number_occupied_slots
-                instance_count_dict[instance_type].hosts_with_min_free_slots_number = 1
-            elif instance_count_dict[instance_type].min_free_slots_number_per_host == slot_number - number_occupied_slots:
-                instance_count_dict[instance_type].hosts_with_min_free_slots_number += 1
-        fr.close()
 
-    with open(file_output_path, 'w') as fa:
-        """
-        EMPTY: M1=<count>; M2=<count>; M3=<count>;
-        FULL: M1=<count>; M2=<count>; M3=<count>;
-        MOST FILLED: M1=<count>,<empty slots>; M2=<count>,<empty slots>; M3=<count>,<empty slots>;
-        """
-        # TODO use format instead of string concatenation
-        string_empty = "EMPTY:M1=" + instance_count_dict['M1'].empty_hosts.__str__() + ";M2=" + instance_count_dict['M2'].empty_hosts.__str__() + ";M3=" + \
-            instance_count_dict['M3'].empty_hosts.__str__() + ';\n'
+    for host_line in fr:
+        host_array = host_line.strip().split(',')
+        # check array length
+        if len(host_array) < 4 and len(host_array) > 0:
+            raise ValueError("too few values for host ID: {0} ".format(host_array[0]))
+        elif len(host_array) < 0:
+            raise ValueError("one record corrupted, check FleetState.txt")
+        instance_type, slot_number= host_array[1], int(host_array[2])
+        try:
+            number_occupied_slots = host_array[3:].count('1')
+        except ValueError:
+            raise ValueError("check values for host ID: {0} ".format(host_array[0]))
+        process_record(number_occupied_slots, slot_number, instance_count_dict[instance_type] )
 
-        string_full = "FULL:M1=" + instance_count_dict['M1'].full_hosts.__str__() + ";M2=" + instance_count_dict['M2'].full_hosts.__str__() + ";M3=" + \
-            instance_count_dict['M3'].full_hosts.__str__() + ';\n'
+    string_empty = "EMPTY: M1={0}; M2={1}; M3={2};\n".format(
+        instance_count_dict['M1'].empty_hosts,
+        instance_count_dict['M2'].empty_hosts,
+        instance_count_dict['M3'].empty_hosts)
 
-        string_most_filled = "MOST FILLED: M1=" + instance_count_dict['M1'].hosts_with_min_free_slots_number.__str__() + "," + instance_count_dict['M1'].min_free_slots_number_per_host.__str__() + ";" + \
-             "M2=" + instance_count_dict['M2'].hosts_with_min_free_slots_number.__str__() + "," + instance_count_dict['M2'].min_free_slots_number_per_host.__str__() + ";" + \
-             "M3=" + instance_count_dict['M3'].hosts_with_min_free_slots_number.__str__() + "," + instance_count_dict['M3'].min_free_slots_number_per_host.__str__() + ';\n'
-        fa.writelines([string_empty, string_full, string_most_filled])
-        fa.close()
+    string_full = "FULL: M1={0}; M2={1}; M3={2};\n".format(
+        instance_count_dict['M1'].full_hosts,
+        instance_count_dict['M2'].full_hosts,
+        instance_count_dict['M3'].full_hosts
+    )
+
+    string_most_filled = "MOST FILLED: M1={0},{1}; M2={2},{3}; M3={4},{5};\n".format(
+        instance_count_dict['M1'].hosts_with_min_free_slots_number,
+        instance_count_dict['M1'].min_free_slots_number_per_host,
+        instance_count_dict['M2'].hosts_with_min_free_slots_number,
+        instance_count_dict['M2'].min_free_slots_number_per_host,
+        instance_count_dict['M3'].hosts_with_min_free_slots_number,
+        instance_count_dict['M3'].min_free_slots_number_per_host
+    )
+    write_func([string_empty, string_full, string_most_filled])
+
     print 'completed!'
+
+def make_stats(file_input_path, file_output_path):
+    if not os.path.exists("./{0}".format(file_input_path)):
+        raise IOError("input file {0} missing".format(file_input_path))
+    if not os.path.exists("./{0}".format(file_output_path)):
+        raise IOError("output file {0} missing".format(file_output_path))
+
+    with open(file_input_path, 'r') as fr:
+        with open(file_output_path, 'w') as fa:
+            analyze_records(fr, lambda x : fa.writelines(x))
+            fa.close()
+        fr.close()
+    pass
 
 if __name__ == '__main__':
     make_stats('./FleetState.txt', './Statistics.txt')
---
+++ unittests.py
@@ -0,0 +1,5 @@
+#test if file no exists
+
+#test if not all kind of instances are present
+
+#each test must create and remove input and output files
