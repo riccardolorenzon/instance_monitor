d9655c0812062747364e806a94abb7d1be124388
riccardo lorenzon <riccardo.lorenzon@gmail.com>
2015-03-11 22:12:43 +0100

    fix

--- instance_monitor.py
+++ instance_monitor.py
@@ -1,10 +1,7 @@
-# todo unittests
-
-#todo use namedvalue instead
-
 from collections import namedtuple
 import os
 
+# represents the instance details, for each instance(M1,M2,M3) contains all details.
 class instance_detail():
     def __init__(self, type_name):
         self.type_name = type_name
@@ -13,18 +10,21 @@ class instance_detail():
         self.min_free_slots_number_per_host = -1
         self.hosts_with_min_free_slots_number = 0
 
+# compute each record and use the instance dict to store the partial result
 def process_record(number_occupied_slots, slot_number, instance_type_count_dict ):
     if number_occupied_slots == 0:
         instance_type_count_dict.empty_hosts += 1
     if number_occupied_slots == slot_number:
+        # case smallest number of empty slots = 0
         instance_type_count_dict.full_hosts += 1
         return
     if instance_type_count_dict.min_free_slots_number_per_host == -1 or \
-        instance_type_count_dict.min_free_slots_number_per_host < slot_number - number_occupied_slots:
+        instance_type_count_dict.min_free_slots_number_per_host > slot_number - number_occupied_slots:
         instance_type_count_dict.min_free_slots_number_per_host, instance_type_count_dict.hosts_with_min_free_slots_number = slot_number - number_occupied_slots, 1
     elif instance_type_count_dict.min_free_slots_number_per_host == slot_number - number_occupied_slots:
         instance_type_count_dict.hosts_with_min_free_slots_number += 1
 
+# given the list of host record lines, process the hosts records and write the result relying on the write_output_lines_func
 def make_stats(input_lines, write_output_lines_func):
     instance_count_dict = {}
 
@@ -35,7 +35,6 @@ def make_stats(input_lines, write_output_lines_func):
     for host_line in input_lines:
         host_array = host_line.strip().split(',')
         len_host_array = len(host_array)
-        # check array length
         if len(host_array) < 4 and len_host_array > 0:
             raise ValueError("too few values for host ID: {0} ".format(host_array[0]))
         elif len_host_array < 0:
@@ -44,10 +43,12 @@ def make_stats(input_lines, write_output_lines_func):
             raise ValueError("slot number is not consistent, check record {0}".format(host_array[0]))
 
         instance_type, slot_number= host_array[1], int(host_array[2])
+
         try:
             number_occupied_slots = host_array[3:].count('1')
         except ValueError:
             raise ValueError("check values for host ID: {0} ".format(host_array[0]))
+
         process_record(number_occupied_slots, slot_number, instance_count_dict[instance_type] )
 
     string_empty = "EMPTY: M1={0}; M2={1}; M3={2};\n".format(
@@ -71,8 +72,7 @@ def make_stats(input_lines, write_output_lines_func):
     )
     write_output_lines_func([string_empty, string_full, string_most_filled])
 
-    print 'completed!'
-
+# make stats starting from a file-stored host records list and storing the result in a different file
 def make_stats_from_file(file_input_path, file_output_path):
     if not os.path.exists("./{0}".format(file_input_path)):
         raise IOError("input file {0} missing".format(file_input_path))
